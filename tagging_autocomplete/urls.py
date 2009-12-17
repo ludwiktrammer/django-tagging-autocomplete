230283ee5c046cf974905f8ed97016edff478130
Ludwik Trammer <ludwik@gmail.com>
2009-12-17 18:25:09 +0000

    Changed variable names from "json" to "list", since json is not used.

--- tagging_autocomplete/urls.py
+++ tagging_autocomplete/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('tagging_autocomplete.views',
-    url(r'^json$', 'list_tags', name='tagging_autocomplete-list'),
+    url(r'^list$', 'list_tags', name='tagging_autocomplete-list'),
 )
--- tagging_autocomplete/widgets.py
+++ tagging_autocomplete/widgets.py
@@ -7,9 +7,9 @@ class TagAutocomplete(Input):
 	input_type = 'text'
 	
 	def render(self, name, value, attrs=None):
-		json_view = reverse('tagging_autocomplete-list')
+		list_view = reverse('tagging_autocomplete-list')
 		html = super(TagAutocomplete, self).render(name, value, attrs)
-		js = u'<script type="text/javascript">jQuery().ready(function() { jQuery("#%s").autocomplete("%s", { multiple: true }); });</script>' % (attrs['id'], json_view)
+		js = u'<script type="text/javascript">jQuery().ready(function() { jQuery("#%s").autocomplete("%s", { multiple: true }); });</script>' % (attrs['id'], list_view)
 		return mark_safe("\n".join([html, js]))
 	
 	class Media:
