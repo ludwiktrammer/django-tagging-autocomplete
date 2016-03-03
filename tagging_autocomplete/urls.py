16091eebd0242782715600df9f6db9596f5797fe
Ludwik Trammer <ludwik@gmail.com>
2016-03-03 13:37:34 +0100

    Drop the deprecated way of specifying url patterns

--- tagging_autocomplete/urls.py
+++ tagging_autocomplete/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 
-urlpatterns = patterns(
+urlpatterns = [
     'tagging_autocomplete.views',
     url(r'^list$', 'list_tags', name='tagging_autocomplete-list'),
-)
+]
